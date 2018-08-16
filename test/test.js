2eeaa89237297e7524ed1ea7050791f6a279d58e
Admin <nzhang@AUC02WN07SHV2T.local>
2018-08-16 15:00:03 -0400

    read twitter api credentials from environmental variables

--- test/test.js
+++ test/test.js
@@ -3,6 +3,7 @@ var chai = require("chai");
 var chaiAsPromised = require("chai-as-promised");
 chai.use(chaiAsPromised);
 var should = chai.should();
+var expect = chai.expect;
 var Twit = require('twit');
 
 var CTRequest = require('../lib/ctreq');
@@ -36,25 +37,30 @@ describe('#ctreq with file cache', function() {
   });
 });
 
-// describe('#ctreq with rate limit', function() {
-//   var req = new CTRequest({
-//     'handler' : backbone_handler,
-//     'ttype'   : 'RateLimiter',
-//     'tparams' : [1, 3000],
-//   });
-//
-//   it('respond with matching records', async function() {
-//     return req.issue(['tms_03']).should.eventually.deep.equal({'output':'tms_03'})
-//       && req.issue(['tms_04']).should.eventually.deep.equal({'output':'tms_04'});
-//   }).timeout(5000);
-// });
+describe('#ctreq with rate limit', function() {
+  var req = new CTRequest({
+    'handler' : backbone_handler,
+    'ttype'   : 'RateLimiter',
+    'tparams' : [1, 3000],
+  });
+
+  it('respond with matching records', async function() {
+    return req.issue(['tms_03']).should.eventually.deep.equal({'output':'tms_03'})
+      && req.issue(['tms_04']).should.eventually.deep.equal({'output':'tms_04'});
+  }).timeout(5000);
+});
 
 describe('#ctreq with Twitter API', function() {
+  expect(process.env.TWITTER_API_KEY).to.be.a('string');
+  expect(process.env.TWITTER_API_SECRET).to.be.a('string');
+  expect(process.env.TWITTER_API_TOKEN).to.be.a('string');
+  expect(process.env.TWITTER_API_TOKEN_SECRET).to.be.a('string');
+
   var T = new Twit({
-    consumer_key:			'***REMOVED***',
-  	consumer_secret:		'***REMOVED***',
-  	access_token:		'***REMOVED***',
-  	access_token_secret:	'***REMOVED***',
+    consumer_key:			    process.env.TWITTER_API_KEY,
+  	consumer_secret:		  process.env.TWITTER_API_SECRET,
+  	access_token:		      process.env.TWITTER_API_TOKEN,
+  	access_token_secret:	process.env.TWITTER_API_TOKEN_SECRET,
     timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
     strictSSL:            true,     // optional - requires SSL certificates to be valid.
   })
